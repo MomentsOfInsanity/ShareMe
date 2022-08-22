29d0d6cee9b0f77719dfe417c83fc6c7132854b1
Raj Bedi <raj.j.bedi@gmail.com>
2022-08-22 10:45:06 -0400

    created extra schemas
    created comment.js, pin.js, postby.js, and save.js and added to the schema.js

--- shareme_backend/schemas/schema.js
+++ shareme_backend/schemas/schema.js
@@ -4,6 +4,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import user from './user'
+import pin from './pin'
+import save from './save'
+import comment from './comment'
+import postedBy from './postedBy'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,6 +18,6 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    user
+    user, pin, save, comment, postedBy
   ]),
 })
